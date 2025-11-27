4e16c0e01d92a980fe3d701b0405b611aaf2902d
Samuel Vasconcelos <89222217+sammvasc@users.noreply.github.com>
2025-11-27 13:04:26 -0300

    Add XP classification for hero levels

---
+++ src/classificador.py
@@ -0,0 +1,21 @@
+nome = "Arthorius"
+xp = 7340
+
+if xp < 1000:
+    nivel = "Ferro"
+elif 1001 <= xp <= 2000:
+    nivel = "Bronze"
+elif 2001 <= xp <= 5000:
+    nivel = "Prata"
+elif 5001 <= xp <= 7000:
+    nivel = "Ouro"
+elif 7001 <= xp <= 8000:
+    nivel = "Platina"
+elif 8001 <= xp <= 9000:
+    nivel = "Ascendente"
+elif 9001 <= xp <= 10000:
+    nivel = "Imortal"
+else:
+    nivel = "Radiante"
+
+print(f"O Herói de nome {nome} está no nível de {nivel}")
